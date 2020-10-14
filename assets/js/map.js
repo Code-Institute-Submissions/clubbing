4ee503774dda5a14fc44b517a43a784e1cb26293
aoshenye <aoshenye@gmail.com>
2020-10-14 15:38:40 +0000

    map rendering and positioining move to the bottom

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,15 +2,13 @@
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 59.334591,
+            lng: 18.063240
         }
     });
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+       { lat: 59.3358, lng: 18.05076},
     ];
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
