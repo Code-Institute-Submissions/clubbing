44929f2e947076512aef301c8b404e8716b23544
aoshenye <aoshenye@gmail.com>
2020-10-21 14:45:53 +0000

    increase size of map and correct formating issues

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,22 +1,28 @@
-   function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 59.334591,
-            lng: 18.063240
-        }
-    });
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var locations = [
-       { lat: 59.3358, lng: 18.05076},
-    ];
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            labels: labels[i % labels.length]
-           
+
+var map;
+var stockholmLocation = {
+    lat: 59.334591,
+    lng: 18.063240
+};
+$(document).ready(function () {
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 3,
+            center: stockholmLocation
         });
-    });
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+        var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var locations = [stockholmLocation];
+        var markers = locations.map(function (location, i) {
+            return new google.maps.Marker({
+                position: location,
+                labels: labels[i % labels.length]
+            });
+        });
+        var markerCluster = new MarkerClusterer(map, markers, {
+            imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        });
+    }
+    initMap();
+});
+
   
