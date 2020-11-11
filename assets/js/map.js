9e8d7fb003c4226ce9cb622e73595361a168a15c
aoshenye <aoshenye@gmail.com>
2020-11-11 17:20:17 +0000

    editing headers/removing, resize zoom on map

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,16 +6,16 @@ var HILMA = {
 $(document).ready(function () {
     function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 10,
+            zoom: 6,
             center: HILMA
         });
         var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         
-        var locations = [
-            {lat: 59.334591,lng: 18.063240},
-            {lat: 59.3365, lng:18.0721},
-            {lat: 59.3184, lng:18.0744},
-            {lat: 59.2917, lng:18.0793}
+        var  locations = [
+            { 'name': 'Hilma', lat: 59.334591,lng: 18.063240},
+            { 'name': 'Spy Bar', lat: 59.3365, lng:18.0721},
+            { 'name': 'Sodra Teatern', lat: 59.3184, lng:18.0744},
+            { 'name': 'Slakthuset', lat: 59.2917, lng:18.0793}
 
         ];
         var markers = locations.map(function (location, i) {
@@ -24,6 +24,8 @@ $(document).ready(function () {
                 labels: labels[i % labels.length]
             });
         });
+
+     
         
         var markerCluster = new MarkerClusterer(map, markers, {
             imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
