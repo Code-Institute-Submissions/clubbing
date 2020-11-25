fd57e8a7ed2e5406c77d4fe9f3b7235195921b31
aoshenye <aoshenye@gmail.com>
2020-11-25 14:48:11 +0000

    removed search bar

--- assets/js/searchbox.js
+++ assets/js/searchbox.js
@@ -1,33 +1,35 @@
 var map;
-var coords = {
-    "lat": 59.334591,
-    "lng": 18.063240,
-    "lat": 59.3365,
-    "lng": 18.0721,
-    "lat": 59.3184,
-    "lng": 18.0744
+var Sodra = {
+     lat: 59.3184, lng:18.0744,               
 };
-
+var Hilma = {
+    lat: 59.334591,lng: 18.063240
+}
 
 
 function createMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: coords,
+        center: Sodra,
         zoom: 18,
         
     });
     marker = new google.maps.Marker({
-        position: coords,
+        position: Sodra,
         animation: google.maps.Animation.DROP,
         map: map,
         label: {
-            text: "Spy Bar",
+            text: "Sodra Teatern",
             color: "black",
             fontWeight: "bold",
             fontSize: "20px",
         }
+        
     });
-    
+
+    //marker clusters//
+   
+
+//call places//
 var request = {
         placeId: generalPlaceInfo.placeId,
         fields: ['name', 'geometry', 'opening_hours']
@@ -53,15 +55,14 @@ function googlePlacesAPI(){
         dataType: 'json',
         data: {
               'type': 'night_clubs',
-            'radius': 1500,
+            'radius': 1000,
             'location': '59.334591,18.063240',
         },
     }
-
-    req = $.ajax(settings);
-    req.done(function(data) {
-        console.info(data);
-    });
+// req = $.ajax(settings);
+  //  req.done(function(data) {
+    //    console.info(data);
+   // });
 }
 
 $(document).ready(function () {
@@ -74,37 +75,48 @@ $(document).ready(function () {
     searchBox.addListener("places_changed", function () {
         let places = searchBox.getPlaces();
 
-        //        service --> places
-        //        request = {
-        //            'radius': 5000,
-        //            'type': 'nigh_club',
-        //        }
-        //        places = service.getPlaces(request)
+                service --> places
+                request = {
+                    'radius': 5000,
+                    'type': 'restaurant',
+                }
+                places = service.getPlaces(request)
 
         if (places.length == 0) {
             return;
         }
 
-        places.forEach(function (result) {
-            console.log(result);
-        })
+//        places.forEach(function (result) {
+//            console.log(result);
+//        })
     });
     
 });
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-};
+//function myFunction() {
+//    var input, filter, ul, li, a, i, txtValue;
+//    input = document.getElementById("myInput");
+//    filter = input.value.toUpperCase();
+//    ul = document.getElementById("myUL");
+//    li = ul.getElementsByTagName("li");
+//    for (i = 0; i < li.length; i++) {
+//        a = li[i].getElementsByTagName("a")[0];
+//        txtValue = a.textContent || a.innerText;
+//        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//            li[i].style.display = "";
+//        } else {
+//            li[i].style.display = "none";
+//        }
+//    }
+//};
 
+$(document).ready(function () {
+
+        var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var locations = [
+            {lat: 59.334591,lng: 18.063240},
+            {lat: 59.3365, lng:18.0721},
+            {lat: 59.3184, lng:18.0744},
+            {lat: 59.2917, lng:18.0793}
+        ];
+         });
+       
