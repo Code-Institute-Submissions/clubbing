1414202042cddac233e534d5a5cdee3deb560efd
aoshenye <aoshenye@gmail.com>
2020-11-30 16:11:26 +0000

    adding accordion collapse to replace cards

--- assets/js/searchbox.js
+++ assets/js/searchbox.js
@@ -135,3 +135,16 @@ function createPhotoMarker(place) {
         })
     });
 }
+
+var horizontalAccordions = $(".accordion.width");
+
+horizontalAccordions.each(function() {
+  var accordion = $(this);
+  var collapse = accordion.find(".collapse");
+  var bodies = collapse.find("> *");
+  accordion.height(accordion.height());  
+  bodies.width(bodies.eq(0).width());
+  collapse.not(".show").each(function() {
+    $(this).parent().find("[data-toggle='collapse']").addClass("collapsed");
+  });
+});
