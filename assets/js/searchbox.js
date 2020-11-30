f680e45b71b012d970c6d83d60b78daa54674173
aoshenye <aoshenye@gmail.com>
2020-11-30 23:07:39 +0000

    remove image for bootstrap collaps

--- assets/js/searchbox.js
+++ assets/js/searchbox.js
@@ -80,6 +80,84 @@ function createMarkersAndUpdateHTML(places, map) {
         });
         
         // Add callback to the marker here
+
+function initMap() {
+
+    var latlng = new google.maps.LatLng(28.535516,77.391026);
+
+    if (navigator.geolocation) {
+      try {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var myLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          setPos(myLocation);
+        });
+      } catch (err) {
+        var myLocation = {
+          lat: 25.2048,
+          lng: 55.2708
+        };
+        setPos(myLocation);
+      }
+    } else {
+      var myLocation = {
+        lat: 25.2048,
+        lng: 55.2708
+      };
+      setPos(myLocation);
+    }
+  }
+
+  function setPos(myLocation) {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: myLocation,
+      zoom: 10
+    });
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: myLocation,
+      radius: 5000,
+      types: ['night_club']
+    }, processResults);
+  }
+
+  function processResults(results, status, pagination) {
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+      return;
+    } else {
+      createMarkers(results);
+    }
+  }
+
+  function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
+    for (var i = 0, place; place = places[i]; i++) 
+    {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        animation: google.maps.Animation.DROP,
+        position: place.geometry.location
+      });
+
+      bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+  }
+
+// To add the marker to the map, call setMap();
+marker.setMap(map);
         
         bounds.extend(place.geometry.location);
     }
