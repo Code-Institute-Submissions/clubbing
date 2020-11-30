e95169fa9a968d85c1bb70d66587f6b31fd3c897
aoshenye <aoshenye@gmail.com>
2020-11-30 16:38:29 +0000

    replace horizontal collapse bootsrap with vertical bootstrap

--- assets/js/searchbox.js
+++ assets/js/searchbox.js
@@ -136,15 +136,21 @@ function createPhotoMarker(place) {
     });
 }
 
-var horizontalAccordions = $(".accordion.width");
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-horizontalAccordions.each(function() {
-  var accordion = $(this);
-  var collapse = accordion.find(".collapse");
-  var bodies = collapse.find("> *");
-  accordion.height(accordion.height());  
-  bodies.width(bodies.eq(0).width());
-  collapse.not(".show").each(function() {
-    $(this).parent().find("[data-toggle='collapse']").addClass("collapsed");
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
   });
-});
+}
